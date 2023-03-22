9bad65202c739f7bc2b91359821206c717be3815
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2023-03-22 19:01:07 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,22 +1,45 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+    test state
+"""
+from models.base_model import BaseModel
 from models.state import State
-import os
+import unittest
 
 
-class test_state(test_basemodel):
-    """ states test class"""
+class test_State(unittest.TestCase):
+    """
+        test for state class
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+            setup
+        """
+        cls.dummy_state = State()
+        cls.dummy_state.name = "tests"
 
-    def __init__(self, *args, **kwargs):
-        """ state test class init"""
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    @classmethod
+    def tearDownClass(cls):
+        """
+            tear down
+        """
+        del cls.dummy_state
 
-    def test_name3(self):
-        """ testing state name attr"""
-        new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+    def test_inheritance(self):
+        """
+            test proper inheritance
+        """
+        self.assertIsInstance(self.dummy_state, BaseModel)
+        self.assertTrue(hasattr(self.dummy_state, "id"))
+        self.assertTrue(hasattr(self.dummy_state, "created_at"))
+        self.assertTrue(hasattr(self.dummy_state, "updated_at"))
+
+    def test_attrs(self):
+        """
+            test attributes
+        """
+        self.assertTrue(hasattr(self.dummy_state, "name"))
+
+if __name__ == "__main__":
+    unittest.main()
