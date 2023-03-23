c16627e6728c8ddd53e2ba4ab4d47e2c478ee28e
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2023-03-23 08:07:15 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
 import MySQLdb
-import unittest
+import pytest
 
-class TestStateFunctions(unittest.TestCase):
-    def setUp(self):
-        # Connect to the test database
-        self.db = MySQLdb.connect(host="localhost", user="testuser", passwd="testpass", db="testdb")
-        self.cursor = self.db.cursor()
+@pytest.fixture
+def db():
+    # Connect to the test database
+    conn = MySQLdb.connect(host="localhost", user="testuser", passwd="testpass", db="testdb")
+    cursor = conn.cursor()
 
-    def test_add_state(self):
-        # Get the number of states before adding a new one
-        self.cursor.execute("SELECT COUNT(*) FROM states")
-        before_count = self.cursor.fetchone()[0]
+    yield cursor
 
-        # Add a new state to the database
-        self.cursor.execute("INSERT INTO states (name) VALUES ('California')")
+    # Clean up after the test
+    conn.rollback()
+    cursor.close()
+    conn.close()
 
-        # Get the number of states after adding the new one
-        self.cursor.execute("SELECT COUNT(*) FROM states")
-        after_count = self.cursor.fetchone()[0]
+def test_add_state(db):
+    # Get the number of states before adding a new one
+    db.execute("SELECT COUNT(*) FROM states")
+    before_count = db.fetchone()[0]
 
-        # Check that the number of states increased by one
-        self.assertEqual(before_count + 1, after_count)
+    # Add a new state to the database
+    db.execute("INSERT INTO states (name) VALUES ('California')")
 
-    def tearDown(self):
-        # Clean up after the test
-        self.db.rollback()
-        self.cursor.close()
-        self.db.close()
+    # Get the number of states after adding the new one
+    db.execute("SELECT COUNT(*) FROM states")
+    after_count = db.fetchone()[0]
 
-if __name__ == '__main__':
-    unittest.main()
+    # Check that the number of states increased by one
+    assert before_count + 1 == after_count
