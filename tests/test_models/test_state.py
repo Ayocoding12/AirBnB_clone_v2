ef0ec7276cafb572b322eb29ff8525d2376f9cac
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2023-03-23 15:22:31 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-import MySQLdb
-import pytest
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.state import State
 
-@pytest.fixture
-def db():
-    # Connect to the test database
-    conn = MySQLdb.connect(host="localhost", user="testuser", passwd="testpass", db="testdb")
-    cursor = conn.cursor()
 
-    yield cursor
+class test_state(test_basemodel):
+    """ """
 
-    # Clean up after the test
-    conn.rollback()
-    cursor.close()
-    conn.close()
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
 
-def test_add_state(db):
-    # Get the number of states before adding a new one
-    db.execute("SELECT COUNT(*) FROM states")
-    before_count = db.fetchone()[0]
-
-    # Add a new state to the database
-    db.execute("INSERT INTO states (name) VALUES ('California')")
-
-    # Get the number of states after adding the new one
-    db.execute("SELECT COUNT(*) FROM states")
-    after_count = db.fetchone()[0]
-
-    # Check that the number of states increased by one
-    assert before_count + 1 == after_count
+    def test_name3(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
