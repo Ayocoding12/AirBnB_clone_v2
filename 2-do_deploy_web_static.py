5cc6ecc1642d6c5113ca6354a06e018885caa9ae
Gorilla <111683116+Ayocoding12@users.noreply.github.com>
2023-04-20 06:29:32 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+# Fabric script (based on the file 1-pack_web_static.py) that distributes
+# an archive to your web servers, using the function do_deploy
+
 import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ['54.224.49.64', '52.91.133.74']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
-
     Args:
         archive_path (str): The path of the archive to distribute.
     Returns:
